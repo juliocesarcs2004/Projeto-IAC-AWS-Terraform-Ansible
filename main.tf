47e9604d1ad5cd7c2787c1a3cdbf9b236ff1f60d
Julio <juliocesarcs2004@gmail.com>
2024-02-27 20:21:00 -0300

    Provisionada máquina com Ubuntu

--- main.tf
+++ main.tf
@@ -17,7 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   key_name = "iac-alura"
+  # user_data = <<-EOF
+  #           #!/bin/bash
+  #           cd /home/ubuntu
+  #           echo "<h1>Feito com Terraform 2</h1>" > index.html
+  #           nohup busybox httpd -f -p 8080 &
+  #             EOF
+  # user_data_replace_on_change = true
   tags = {
-    Name = "Primeira instancia"
+    Name = "Teste aws"
   }
 }
